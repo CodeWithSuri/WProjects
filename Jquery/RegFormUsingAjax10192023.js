494cf7dd5db2fb8a9a8a026426958ac79f55cbbf
Code With Suri <surilsk4@gmail.com>
2023-12-09 16:38:32 +0530

    Jquery Ajax &Regular expressions
    Jquery Ajax &Regular expressions

--- Jquery/RegFormUsingAjax10192023.js
+++ Jquery/RegFormUsingAjax10192023.js
@@ -83,10 +83,110 @@ var countrieslistFromjson=[];
     if(pattern.test($(this).val())){
         $("#errorFirstname").hide();
     }else{
-        $("#errorFirstname").show().text("Username must have  one small char and one number one sepecial char -_ also Username will be min 5 and max 8");
+        $("#errorFirstname").show().text("First name must have  one small char and one number one sepecial char -_ also Username will be min 5 and max 8");
     }
 
     });
+
+
+    $("#lnameid").on("keyup",function(){
+
+
+        var pattern = /^[a-z0-9_-]{4,6}$/;
+    if(pattern.test($(this).val())){
+        $("#errorLastname").hide();
+    }else{
+        $("#errorLastname").show().text("Last must have  one small char and one number one sepecial char -_ also Username will be min 4 and max 6");
+    }
+
+    });
+
+
+    $("#emailid").on("change",function(){
+
+
+        var pattern = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    if(pattern.test($(this).val())){
+        $("#errorEmail").hide();
+    }else{
+        $("#errorEmail").show().text("Please enter valid email address");
+    }
+
+    });
+
+
+    
+    $("#phnoid").on("keydown",function(){
+
+
+        var pattern = /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+    if(pattern.test($(this).val())){
+        $("#errorPhno").hide();
+    }else{
+        $("#errorPhno").show().text("Please enter valid phone number");
+    }
+
+    });
+
+
+    $("#addressid").on("change",function(){
+
+
+        var pattern = /^[#.0-9a-zA-Z\s, ,-]+$/;
+    if(pattern.test($(this).val())){
+        $("#errorAddress").hide();
+    }else{
+        $("#errorAddress").show().text("Please enter valid address");
+    }
+
+    });
+
+    
+    $("#Socialnetworklinkid").on("keyup",function(){
+
+
+        var pattern = /^(http(s)?:\/\/)?(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+    if(pattern.test($(this).val())){
+        $("#errorSocialNetworklink").hide();
+    }else{
+        $("#errorSocialNetworklink").show().text("Please enter valid social link");
+    }
+
+    });
+
+    
+    $("#passwordid").on("keyup",function(){
+
+
+        var pattern =/^(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,}$/; 
+           if(pattern.test($(this).val())){
+        $("#errorpassword").hide();
+    }else{
+        $("#errorpassword").show().text("Please enter valid password");
+    }
+
+    });
+
+
+        
+    $("#cnpasswordid").on("keyup",function(){
+
+
+        var pwdVal=  $("#passwordid").val();
+        
+        var cnpwdVal=  $("#cnpasswordid").val();
+
+        if(pwdVal ==cnpwdVal ){
+        $("#errorcnpassword").hide();
+    }else{
+        $("#errorcnpassword").show().text("Confrim password is not match");
+    }
+
+    });
+
+    
+
+   
 
 
     $("#regFormid").on("submit",function(event){
@@ -103,6 +203,8 @@ var countrieslistFromjson=[];
         var phno =$("#phnoid").val();
         var address =$("#addressid").val();
         var socaillink =$("#Socialnetworklinkid").val();
+        var pwd =$("#passwordid").val();
+        var cnpwd =$("#cnpasswordid").val();
         var hobbies =getcheckboxSelectedValues();
         var gender =$("input[name='gender']:checked").val();
         var countries =$("#countrydropdownid").val();
@@ -150,6 +252,22 @@ var countrieslistFromjson=[];
             $("#errorSocialNetworklink").show();
         }else{
             $("#errorSocialNetworklink").hide();
+        }
+
+        
+        if(pwd==""){
+
+            $("#errorpassword").show();
+        }else{
+            $("#errorpassword").hide();
+        }
+
+        
+        if(cnpwd==""){
+
+            $("#errorcnpassword").show();
+        }else{
+            $("#errorcnpassword").hide();
         }
         
         if(gender==undefined || gender=='undefined'){
